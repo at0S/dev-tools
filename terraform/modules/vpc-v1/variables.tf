ec5c73dcd76f1a1fae7940847da33468718f2892
Taras Yermolenko <t.yermolenko@gmail.com>
2020-11-03 18:30:05 +1100

    terraform: passing default region

--- terraform/modules/vpc-v1/variables.tf
+++ terraform/modules/vpc-v1/variables.tf
@@ -1,23 +1,4 @@
-variable "tenant" {
-    description = "Used to identify who the resources belongs to, mainly in name tags"
-    type = string
-}
-
-variable "environment" {
-    description = "which environment resource belongs too, like 'development', 'production', etc"
-}
-
-variable "cidr" {
-    description = "CIDR block for VPC. Must be within 16 - 28 bit mask"
-    type = string
-}
-
-variable "subnet_tags" {
-    description = "Additional tags for a subnet"
-    default = {}
-}
-
-variable "suffix" {
-    description = "random sequence, if empty(default) will generate its own value"
-    default = ""
+variable "region" {
+  default     = "ap-southeast-2"
+  description = "AWS region. Default is Sydney"
 }

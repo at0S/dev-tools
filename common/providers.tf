a4d16cf3acc77973b2eee3794ecc46528db2039e
Taras Yermolenko <t.yermolenko@gmail.com>
2020-11-03 18:28:33 +1100

    terraform: common resources, namely VPC and EKS cluster and its nodes

--- common/providers.tf
+++ common/providers.tf
@@ -9,3 +9,11 @@ provider "aws" {
     alias = "ap-southeast-1"
     region = "ap-southeast-1"
 }
+
+provider "kubernetes" {
+  version                = "1.13.2" 
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
